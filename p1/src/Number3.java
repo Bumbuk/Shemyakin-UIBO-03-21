2f2967c42d81bf8bc5ce6f1eaa95655bb835e176
Bumbuk <shemyakin.03@bk.ru>
2023-09-07 15:27:28 +0300

    done

---
+++ p1/src/Number3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Number3 {
+    public static void main(String[] args) {//номер 3
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов в массиве");
+        int size = sc.nextInt();
+        System.out.println("Введите элементы массива");
+        int[] arr = new int[size] ;
+        int sum = 0;
+        for(int i = 0;i<size;i++){
+           arr[i] = sc.nextInt();
+           sum+=arr[i];
+        }
+        System.out.println("Сумма элементов массива(for) = " + sum);
+        sum = 0;
+
+        int i = 0;
+        while(i<size){
+            sum+=arr[i];
+            i++;
+        }
+        System.out.println("Сумма элементов массива(while) = " + sum);
+        sum = 0;
+
+        int j = 0;
+        do{
+            sum+=arr[j];
+            j++;
+        }while(j<size);
+        System.out.println("Сумма элементов массива(do while) = " + sum);
+    }//номер 3
+}
---
+++ p1/src/Number7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = sc.nextInt();
+        int fact = 1;
+        for(int i = 1;i<=number;i++){
+           fact*=i;
+        }
+        System.out.println("Факторил числа "+ number + " = " + fact);
+    }
+}
