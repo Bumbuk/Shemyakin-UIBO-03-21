081996cc5860498675aebc075a685094b44c1166
Bumbuk <144329036+Bumbuk@users.noreply.github.com>
2023-09-27 19:34:18 +0300

    Add files via upload

---
+++ Java practice 3/src/pr3/Square.java
@@ -0,0 +1,9 @@
+package pr3;
+
+public class Square extends Rectangle{
+
+    public Square(String color, boolean filled, double side1, double side) {
+
+        super(color, filled, side1, side);
+    }
+}
