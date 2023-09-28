cd39db6f354bbb965e3af2fa953105089c656253
Bumbuk <144329036+Bumbuk@users.noreply.github.com>
2023-09-28 09:45:29 +0300

    Delete Java practice 3 directory

--- Java practice 3/src/pr3/Square.java
+++
@@ -1,9 +0,0 @@
-package pr3;
-
-public class Square extends Rectangle{
-
-    public Square(String color, boolean filled, double side1, double side) {
-
-        super(color, filled, side1, side);
-    }
-}
